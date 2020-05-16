80daf1fac07f656597d98655940c00aad5b49d97
charliegabriella <charlotkroon@gmail.com>
2020-05-16 21:20:36 +0200

    App: Add name

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Tree from "./components/Tree";
 
 function App() {
   return (
     <div>
       {" "}
-      <Tree />{" "}
+      <Tree name "Charlie" />
     </div>
   );
 }
