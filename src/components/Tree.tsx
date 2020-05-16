1ea6a1c5261f5c21bb1dbda4071aa070804c348e
charliegabriella <charlotkroon@gmail.com>
2020-05-16 21:39:06 +0200

    Accept Merge

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -2,23 +2,16 @@ import React from "react";
 
 type Props = {
   name: string;
-<<<<<<< HEAD
   scientificName: string;
-=======
->>>>>>> 0482a811ea8f4633e328001e220abcafc93f9786
 };
 
 class Tree extends React.Component<Props> {
   render() {
-<<<<<<< HEAD
     return (
       <p>
         Name: {this.props.name} scientificName:{this.props.scientificName}
       </p>
     );
-=======
-    return <p>Name: {this.props.name}</p>;
->>>>>>> 0482a811ea8f4633e328001e220abcafc93f9786
   }
 }
 
