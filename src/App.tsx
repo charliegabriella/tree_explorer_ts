030cdca5d136667b154e24e4d3e3c5a0a4d4c186
charliegabriella <charlotkroon@gmail.com>
2020-05-17 09:26:35 +0200

    Add PopularTrees to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Tree from "./components/Tree";
+import PopularTrees from "./components/PopularTrees";
 
 function App() {
   return (
     <div>
       {" "}
+      <PopularTrees />
       <Tree name={"Charlita"} />
       <Tree name={"White birch"} scientificName={"Betula pendula"} />
       <Tree name={"Weeping willow"} scientificName={"Salix sepulcralis"} />
